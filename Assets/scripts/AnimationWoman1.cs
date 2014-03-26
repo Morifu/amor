7004650c7211eaed670042733dcb629f73bac60b
ak84c <andrzej.k.chrzanowski@gmail.com>
2014-03-27 00:28:00 +0100

    prefab + animacja kobiety

---
+++ Assets/scripts/AnimationWoman1.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationWoman1 : MonoBehaviour {
+	public float maxSpeed = 1f;
+	bool facingRight = true;
+	public float move;
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//float move = Input.GetAxis("Horizontal");
+
+		anim.SetFloat("Speed", Mathf.Abs (move));
+
+		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+
+		if (move> 0 && facingRight) 
+			Flip ();
+		else if (move< 0 && !facingRight) 
+			Flip ();
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *=-1;
+		transform.localScale = theScale;
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Arrow") {
+			move = -1;
+		}
+		else {
+			move = 0;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
