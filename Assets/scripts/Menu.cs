a2c912d766fa4cccdf7ec33fd86e65cc9daa65bb
ak84c <andrzej.k.chrzanowski@gmail.com>
2014-04-03 09:40:26 +0200

    added lv7 coliders menu in progress

---
+++ Assets/scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+	public string where;
+	bool travelNow = false;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (travelNow == true) {
+			travelNow = false;
+			Application.LoadLevel(where);
+		}
+
+	
+	}
+
+	void OnMouseDown() {
+		travelNow=true;
+
+	}
+}
