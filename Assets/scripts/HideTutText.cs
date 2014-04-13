8c47d32edca0244937a6092f55b7e80469d6aa02
ak84c <andrzej.k.chrzanowski@gmail.com>
2014-04-13 10:53:13 +0200

    lv2 <-> lv3

---
+++ Assets/scripts/HideTutText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideTutText : MonoBehaviour {
+	public int timeToHide;
+	int timeLeft;
+	int fpsToHide;
+	// Use this for initialization
+	void Start () {
+		timeLeft=0;
+		fpsToHide=timeToHide*60;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft++;
+	if (fpsToHide < timeLeft)
+			Destroy (gameObject);
+	}
+}
