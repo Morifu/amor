1d7844955abf40a8ef7c7bdd21da2603b74a3b55
ak84c <andrzej.k.chrzanowski@gmail.com>
2014-04-24 07:35:16 +0200

    added sounds to project

---
+++ Assets/scripts/SiteLock.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SiteLock : MonoBehaviour {
+	/*
+	// Use this for initialization
+	void Start () {
+		Application.ExternalEval("if(document.location.host != 'http://orohimaru.zebromalz.info/amor/') { document.location=''; }");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	*/
+}
