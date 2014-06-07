e8615efa5e7b371afd3b2985211599d583613127
ak84c <andrzej.k.chrzanowski@gmail.com>
2014-06-07 12:09:32 +0200

    fix

--- Assets/scripts/SiteLock.cs
+++ Assets/scripts/SiteLock.cs
@@ -10,29 +10,32 @@ public class SiteLock : MonoBehaviour {
 		/*Application.ExternalEval("if(document.location.host != 'http://orohimaru.zebromalz.info/amor/') { document.location=''; }");*/
 
 		bool isPirated = true;
-		
+		/*
 		if (Application.isWebPlayer) {
 
 		//   
-			/*
+
 		if (Application.absoluteURL.Contains("http://www.flashgamelicense.com"))
 			{ isPirated = false; }
 		if (Application.absoluteURL.Contains("https://www.flashgamelicense.com"))
 		{ isPirated = false; }
-		//if (Application.absoluteURL.Contains("http://andrzejchrzanowski.home.pl")) 
-		//{ isPirated = false; }
+		if (Application.absoluteURL.Contains("http://andrzejchrzanowski.home.pl")) 
+		{ isPirated = false; }
 
 		if (isPirated == true)
 				Application.LoadLevel("blockPirate");
 				//print("Pirated web player");
 		else if (isPirated == false)
+
+			{}
 				//Application.LoadLevel("mainMenu");
 
-		*/		
+			
 			//	
 
 
 }
+		*/
 
 	}
 
